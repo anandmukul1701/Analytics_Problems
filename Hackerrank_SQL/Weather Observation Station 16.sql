ad8651c99ea93bcbad66e775fa05c92bf175672b
Mukul Anand <mailonmukul@gmail.com>
2022-03-26 16:48:49 +0530

    Weather Observation Station 16

---
+++ Hackerrank_SQL/Weather Observation Station 16.sql
@@ -0,0 +1,7 @@
+Weather Observation Station 16
+
+select round(min(lat_n),4)
+from station where lat_n > 38.778
+;
+
+---https://www.hackerrank.com/challenges/weather-observation-station-16/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
