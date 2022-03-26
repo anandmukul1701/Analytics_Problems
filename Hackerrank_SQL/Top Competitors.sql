eca40c5534d42e608967a18a1247aaba7f6646bb
Mukul Anand <mailonmukul@gmail.com>
2022-03-27 01:30:12 +0530

    Top Competitors

--- Hackerrank_SQL/Top Competitors.sql
+++ Hackerrank_SQL/Top Competitors.sql
@@ -1,5 +1,28 @@
 
 
+--https://www.hackerrank.com/challenges/full-score/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+
+
+select h.hacker_id, h.name 
+from Submissions as s 
+join Hackers as h 
+on s.hacker_id = h.hacker_id 
+join Challenges as c 
+on s.challenge_id = c.challenge_id
+join Difficulty as d 
+on c.Difficulty_level = d.Difficulty_level
+where s.score = d.score 
+group by h.hacker_id, h.name 
+having count(*) > 1
+order by count(*) desc, h.hacker_id
+;
+
+
+
+
+
+
 
 
 with base as(
@@ -21,6 +44,9 @@ where S.score = D.score
 
 select concat(hacker_id, ' ', name)
 from base 
-where hacker_id in (select distinct hacker_id from(select hacker_id, count(distinct challenge_id) as cnt from base group by 1 having cnt > 1))
+where hacker_id in (select distinct hacker_id 
+					from(
+						select hacker_id, count(distinct challenge_id) as cnt from base group by 1 having cnt > 1)
+					)
 ;
 
